3067e3d3480d8d98f27f126b917f3c8f69d65c78
StephenOnyekachi <88212958+StephenOnyekachi@users.noreply.github.com>
2026-06-30 07:12:56 +0100

    Fix settings.py syntax error

--- Project/settings.py
+++ Project/settings.py
@@ -163,16 +163,16 @@ DEFAULT_FROM_EMAIL = os.getenv("DEFAULT_FROM_EMAIL", "onboarding@resend.dev")
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 
-redis cache configuration
-CACHES = {
-    "default": {
-        "BACKEND": "django_redis.cache.RedisCache",
-        "LOCATION": os.environ.get("REDIS_URL", "redis://127.0.0.1:6379/1"),
-        "OPTIONS": {
-            "CLIENT_CLASS": "django_redis.client.DefaultClient",
-        }
-    }
-}
+# redis cache configuration
+# CACHES = {
+#     "default": {
+#         "BACKEND": "django_redis.cache.RedisCache",
+#         "LOCATION": os.environ.get("REDIS_URL", "redis://127.0.0.1:6379/1"),
+#         "OPTIONS": {
+#             "CLIENT_CLASS": "django_redis.client.DefaultClient",
+#         }
+#     }
+# }
 
 # Caches
 CACHES = {
